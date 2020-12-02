6c66cad279897761503f473322bc9f48dc537312
Leeza Sergeeva <sergeevaleeza@gmail.com>
2020-12-02 23:13:24 +0000

    Add first part of methods section in Rmd file, update references, add date param to render_rmd.sh

--- code/14_render_rmd.sh
+++ code/14_render_rmd.sh
@@ -2,26 +2,32 @@
 
 set -euo pipefail
 
+# Edited on December 1, 2020 to take an additional argument.
 # A bash script to drive the rendering of an Rmarkdown file
-# it requires 4 arguments, as described below
+# it requires 5 arguments, as described below
 
 # Naupaka Zimmerman
 # nzimmerman@usfca.edu
 # November 1, 2020
 
-if [ $# -ne 4 ]
+# Leeza Sergeeva
+# esergeeva@usfca.edu
+# December 2, 2020
+
+if [ $# -ne 5 ]
 then
   echo "To run this script, supply four arguments:"
   echo "The first should be the name of the Rmd file to be rendered"
   echo "The second should be the path to the gff annotation file"
   echo "The third should be the path to the directory of processed vcf files"
   echo "The fourth should be the path to the SRA run table containing sample metadata"
+  echo "The fifth should be the date in format YYYYMMDD"
   exit 1
 fi
 
 # Set input params for the render command
 RMD_FILE="$1"
-RMD_PARAMS="params = list(gff_file_path = '$2', vcf_dir_path = '$3', sra_runtable_path = '$4')"
+RMD_PARAMS="params = list(gff_file_path = '$2', vcf_dir_path = '$3', sra_runtable_path = '$4', chosen_date = '$5')"
 RMD_OUTPUT="output_dir = 'output'"
 
 # the single quotes around $RMD_FILE are key otherwise R thinks it's an object instead of a file
--- code/functions/state_covid_death.R
+++ code/functions/state_covid_death.R
@@ -1,5 +1,5 @@
 # script to gain inside knowledge of the data for all US states
-# using dplyr package to get number of total deaths by state 
+# using dplyr package to get number of total deaths by state
 # and save it as .csv file
 
 # Leeza Sergeeva
@@ -8,45 +8,33 @@
 
 # load the package "dplyr"
 
-state_covid_death <- function() {
+library(dplyr)
+library(ggplot2)
+library(grid)
+require(grid)
+#chosen_date <- "20201130"
+
+state_covid_death <- function(chosen_date) {
   # import new data as .csv from website API
-  covid_tracking_data <- readr::read_csv("https://api.covidtracking.com/v1/states/daily.csv")
+  covid_tracking_data <- readr::read_csv(
+    "https://api.covidtracking.com/v1/states/daily.csv")
   
-  # dplyr chians, get summary for state date mobility grouped by geo_type and
-  # transportation_type as a table.
+  # dplyr chains, extract date, state and death columns      
   state_death_summary <- covid_tracking_data %>%
-    select(date, state, death, positive, negative, totalTestResults)
-    tally()
+    select(date, state, death)
+  tally()
   
-  # number of total deaths by state
-  total_state_death <- aggregate(death~state, state_death_summary, sum)
-  names(total_state_death)[2] <- 'total_death'
+  # take the input date and select rows matching it     
+  state_death_chosen_date <- state_death_summary %>%        
+    dplyr::filter(`date` == chosen_date)
   
-  # reorder data to show the states with the most deaths first
-  total_state_death_ordered <- total_state_death[
-    order(total_state_death$total_death, decreasing=TRUE), ]
+  # reorder data to show the states with the most deaths first 
+  state_death_chosen_date_ordered <- state_death_chosen_date[
+    order(state_death_chosen_date$death, decreasing=TRUE), ]
   
+  # take top 10 states
+  state_death_chosen_date_ten <- state_death_chosen_date_ordered %>%
+    top_n(10)
   
-  # save table of Covid-19 total deaths by state as a .csv file
-  readr::write_csv(total_state_death_ordered,
-                   paste0("data/metadata/total_state_death_ordered.csv"))
-  
-  return(total_state_death_ordered)
+  return(state_death_chosen_date_ten)
 }
-
-
-#plot(death_total_plot$total_death)
-
-#death_total_plot <- ggplot(data = total_state_death_ordered,
-#                         aes(x = total_death,
-#                             y = state)) +
-#  scale_x_log10() +
-#  geom_col() +
-#  labs(title = "Number of Covid-19 Related Deaths by State",
-#       x = "Total Deaths",
-#       y = "State Name")
-#
-#ggsave(plot = death_total_plot,
-#       filename = "data/images/death_total_plot.png")
-#
-#death_total_plot
